5158bdc7133e449a4856a73a8d9f8b30218cd8a1
Georgios <geor.oikonomopoulos@gmail.com>
2024-10-17 23:50:09 +0300

    added cors lib

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ const bodyParser = require('body-parser');
 const booksRouter = require('./routes/books');
 const customerRoutes = require('./routes/customers');
 const reservationRoutes = require('./routes/reservations'); 
+const cors = require('cors')
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
